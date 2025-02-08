101203495f0b143517e5ae2bb157108da9be87cf
Molnia00 <bilankati@gmail.com>
2025-02-08 16:27:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   
   useEffect(() => {
     const fetchPhotos = async (query, page) => {
-      const const response = await axios.get("https://api.unsplash.com/search/photos", {
+      const  response = await axios.get("https://api.unsplash.com/search/photos", {
         params: {
           query: query,
           client_id: YOUR_ACCESS_KEY,
